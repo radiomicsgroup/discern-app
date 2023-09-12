bcb31aad6622df920ff30ca1b873758175227225
alonso <alonsogarcia@vhio.net>
2023-09-12 17:08:51 +0000

    better default paths

--- settings.py
+++ settings.py
@@ -8,11 +8,12 @@ from decouple import config
 
 LOG_FOLDER= config('LOG_FOLDER', default="./logs/" )
 
-SLICER_EXEC = config('SLICER_EXEC', default="./programs/Slicer-4.8.1-linux-amd64/Slicer" )
-EMSEGMENTCOMMANDLINE = config('EMSEGMENTCOMMANDLINE', default="./programs/Slicer-4.8.1-linux-amd64/lib/Slicer-4.8/cli-modules/EMSegmentCommandLine" )
-MRML= config('MRML', default="./programs/Slicer-4.8.1-linux-amd64/share/Slicer-4.8/qt-loadable-modules/EMSegment/Tasks/MRI-Human-Brain-Hemisphere.mrml" )
+SLICER_ROOT = config('SLICER_ROOT', default="./programs/Slicer-4.8.1-linux-amd64" )
+DCM2NIIX=config('DCM2NIIX', default="./programs/dcm2niix/dcm2niix" )
 
-DCM2NIIX=config('DCM2NIIX', default="./programs/dcm2niix" )
+SLICER_EXEC = config('SLICER_EXEC', default=os.path.join(SLICER_ROOT,"Slicer"))
+EMSEGMENTCOMMANDLINE = config('EMSEGMENTCOMMANDLINE', default=os.path.join(SLICER_ROOT,"lib","Slicer-4.8","cli-modules","EMSegmentCommandLine"))
+MRML= config('MRML', default=os.path.join(SLICER_ROOT,"share","Slicer-4.8","qt-loadable-modules","EMSegment","Tasks","MRI-Human-Brain-Hemisphere.mrml"))
 
 MODEL1 = config('MODEL1', default="./models/lym_current" )
 MODEL2 = config('MODEL2', default="./models/gbmet_current" )
